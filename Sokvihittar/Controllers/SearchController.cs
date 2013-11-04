d2b75d9868b10a9a95642da476e1b3d8e2aaea08
Rashid Lepshokov <rashid.lepshokov@gmail.com>
2013-11-04 14:04:27 +0100

    created web api preoject

---
+++ Sokvihittar/Controllers/SearchController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Sokvihittar.Controllers
+{
+    public class SearchController : ApiController
+    {
+        // GET /api/search/?text=bmv x5
+        public string Get(string text)
+        {
+            // тут надо вызов твоей бибилиотеки вставить
+            return "Search results for : " + text;
+        }
+    }
+}
