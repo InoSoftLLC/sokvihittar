797cfb1bd8dd0586df3853dc8297d5b505b62832
Rashid Lepshokov <rashid.lepshokov@gmail.com>
2013-11-07 18:36:30 +0100

    fixed some styles

--- Sokvihittar/Controllers/SearchController.cs
+++ Sokvihittar/Controllers/SearchController.cs
@@ -3,9 +3,18 @@ using AllaannonserCrawler;
 
 namespace Sokvihittar.Controllers
 {
+    /// <summary>
+    /// Search API
+    /// </summary>
     public class SearchController : ApiController
     {
         // GET /api/search/?text=bmv x5
+        /// <summary>
+        /// Searches for items on http://www.allaannonser.se
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
         public ProductInfo[] Get(string text, int limit)
         {
             // тут надо вызов твоей бибилиотеки вставить
