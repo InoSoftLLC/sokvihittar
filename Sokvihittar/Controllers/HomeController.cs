4ff5a60086abb28cfcdd9f96567dd458a4e1d924
Rashid Lepshokov <rashid.lepshokov@gmail.com>
2013-11-21 14:27:44 +0400

    implemented draft of the test page
    added Execution time(secs) to each crawler to show on test page

--- Sokvihittar/Controllers/HomeController.cs
+++ Sokvihittar/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Sokvihittar.Crawlers;
+using Sokvihittar.Crawlers.Common;
 
 namespace Sokvihittar.Controllers
 {
@@ -12,9 +14,16 @@ namespace Sokvihittar.Controllers
         {
             return View();
         }
-        public ActionResult Test()
+        public ActionResult Test(string searchText, int? limit)
         {
-            return View();
+            if (!limit.HasValue)
+            {
+                return View(new CrawlerResult[] { });
+            }
+
+            ViewBag.SearchText = searchText;
+            ViewBag.Limit = limit;
+            return View(Crawler.Search(searchText, limit.Value));
         }
     }
 }
